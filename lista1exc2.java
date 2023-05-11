8c8dc32da909e2741c6f9195027362b2b5771825
Igor Daniel <72262654+IgorDanielS@users.noreply.github.com>
2023-05-11 17:36:27 -0300

    Add files via upload

---
+++ lista1exc2.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.listajava;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Admin
+ */
+public class lista1exc2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner dado;
+        float a,b,c;
+        
+        dado = new Scanner(System.in);
+        System.out.print("Digite um numero: ");
+        a = dado.nextFloat();
+        System.out.print("Digite outro numero: ");
+        b = dado.nextFloat();
+        System.out.print("Digite outro numero: ");
+        c = dado.nextFloat();
+        
+        if(a+b < c){
+            System.out.print("O terceiro valor é maior que os 2 primeiros! ");
+        }else{
+            System.out.print("O terceiro valor é menor que os 2 primeiros! ");
+        }
+    }
+    
+}
