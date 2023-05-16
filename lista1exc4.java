ebb6dac7e6c7aa0816d7e9a634574a3745330aa0
Igor Daniel <72262654+IgorDanielS@users.noreply.github.com>
2023-05-16 12:03:40 -0300

    Create lista1exc4.java

---
+++ lista1exc4.java
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.Scanner; 
+public class Main
+
+{
+	public static void main(String[] args) {
+	    float n1;
+	    Scanner dados;
+	    dados = new Scanner(System.in);
+	    
+	    System.out.print("Digite um numero: ");
+	    n1 = dados.nextFloat();
+	    if ( n1 > 0 ){
+	        n1 = n1 * 2;
+	       System.out.print("O dobro do valor é: " + n1 );
+	    }else{
+	        n1 = n1 * 3;
+	        System.out.print("O triplo do valor é: " + n1);
+	    }
+	        
+	}
+}
