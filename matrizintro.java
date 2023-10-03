40eb51a8a5a017f6f35bb93ac7375e80c842d2bd
Igor Daniel <72262654+IgorDanielS@users.noreply.github.com>
2023-10-03 10:33:12 -0300

    Create matrizintro.java

---
+++ matrizintro.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class matriz{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        //declaração da matriz:([L][C])
+        double[][] matriz = new double[3][4];
+        System.out.println("Preenchimento da matriz");
+        preencherMatriz(matriz, input);
+        System.out.println("Exibição da matriz: ");
+        exibirMatriz(matriz);
+    }
+
+    public static void preencherMatriz(double[][] mat, Scanner input) {
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[i].length; j++) {
+                System.out.println("Digite o elemento da matriz: ");
+                try {
+                    mat[i][j] = input.nextDouble();
+                } catch (java.util.InputMismatchException e) {
+                    System.out.println("Entrada inválida. Digite um número válido.");
+                    input.nextLine(); // Limpar o buffer do scanner
+                    j--; // Ficar na mesma posição do loop
+                }
+            }
+        }
+    }
+
+    public static void exibirMatriz(double[][] mat) {
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[i].length; j++) {
+                System.out.print(mat[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
