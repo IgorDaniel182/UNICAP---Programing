885508c730c231ed3eaae7294727ea28e9efa450
Igor Daniel <igordaniel1903@gmail.com>
2024-02-18 10:41:43 -0300

    Metodo de trocar 2 variáveis sem utilizar uma temp adicionado

--- listaRevisao/trocaValores.java
+++ listaRevisao/trocaValores.java
@@ -3,5 +3,19 @@ public class trocaValores
     public static void main(String[]args)
     {
         
+        int a = 2;
+        int b = 3;
+
+        // método XOR para a troca dos valores:
+        a = a^b;
+        b = b^a;
+        a = a^b;
+
+        System.out.print(a + " " + b);
+
+    }   
+    public static void trocaValores(int num1, int num2)
+    {
+        
     }
 }
