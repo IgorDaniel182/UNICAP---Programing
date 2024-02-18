c103117a59968a3046579e7d6043cc158de6b3e8
Igor Daniel <igordaniel1903@gmail.com>
2024-02-18 09:46:14 -0300

    Impar ou Par adicionado

---
+++ listaRevisao/imparPar.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class imparPar
+{
+    public static void main(String[] args)
+    {
+        Scanner leitor = new Scanner(System.in);
+        System.out.print("Digite um numero para verificar se é par ou ímpar: ");
+        int numero = leitor.nextInt();
+
+        String resposta = verificaImparouPar(numero);
+
+        System.out.print("O número é: " + resposta);
+    }
+    public static String verificaImparouPar(int numero)
+    {
+        String resposta;
+        if(numero % 2 == 0)
+        {
+            resposta = "Par";
+        }else
+        {
+            resposta = "Ímpar";
+        }
+        return resposta;
+    }
+}
