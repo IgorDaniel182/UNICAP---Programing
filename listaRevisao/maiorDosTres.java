a80d534df518fc6434404f3605aceed76b267417
Igor Daniel <igordaniel1903@gmail.com>
2024-02-18 14:38:20 -0300

    Verificar o maior número.

---
+++ listaRevisao/maiorDosTres.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+public class maiorDosTres {
+    public static void main(String[] args)
+    {
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.print("Digite o 1° número: ");
+        double num1 = leitor.nextDouble();
+        System.out.print("Digite o 2° número: ");
+        double num2 = leitor.nextDouble();
+        System.out.print("Digite o 3° número: ");
+        double num3 = leitor.nextDouble();
+
+        MaiorNumero(num1,num2,num3);
+
+        leitor.close();
+    }
+    public static void MaiorNumero(double n1, double n2, double n3)
+    {
+        if(n1 > n2 && n1 > n3 )
+        {
+            System.out.print("O maior número é o " + n1);
+        }
+        else if(n2>n1 && n2>n3)
+        {
+            System.out.print("O maior número é o " + n2);
+        }
+        else if(n3>n1 && n3>n2)
+        {
+            System.out.print("O maior número é o " + n3);
+        }
+        else if(n1 == n2 && n1 == n3)
+        {
+            System.out.print("Os números são iguais. ");
+        }
+        else
+        {
+            System.out.print("Erro");
+        }
+    }
+}
