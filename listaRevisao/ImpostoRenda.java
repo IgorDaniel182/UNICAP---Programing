2054509e8d37ca3692c7c0824e82b36e19f468f7
Igor Daniel <igordaniel1903@gmail.com>
2024-02-23 14:25:49 -0300

    ImpostoRenda adicionado

---
+++ listaRevisao/ImpostoRenda.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class ImpostoRenda {
+    public static void main(String[] args) {
+        double salarioAnual, imposto;
+        int faixa;
+        Scanner leitor = new Scanner(System.in);
+        System.out.print("Salario anual: ");
+        salarioAnual = leitor.nextInt();
+        System.out.print("Faixa : ");
+        faixa = leitor.nextInt();
+
+        imposto = calculaImposto(faixa, salarioAnual);
+
+        System.out.println("Imposto a ser pago: "+ imposto);
+
+    }   
+    static double calculaImposto(int escolha, double salario)
+    {
+        double imposto = 0;
+
+        switch (escolha) {
+            case 1:
+            imposto = salario * 0.12;
+            break;        
+            case 2:
+            imposto = salario * 0.13;
+            break;
+            case 3:
+            imposto = salario * 0.15;
+            break;
+        }
+        return imposto;
+    }
+}
---
+++ listaRevisao/Planotelefonia.java
@@ -0,0 +1,37 @@
+import java.math.*;
+import java.util.Scanner;
+public class Planotelefonia {
+    public static void main(String[] args) {
+        
+        double plan1 = 0, plan2 = 0, plan3 = 0;
+        int minConsumo;
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.print("Quanto é o seu consumo telefônico mensal: ");
+        minConsumo = leitor.nextInt();
+
+        calculoPlanos(plan1, plan2, plan3, minConsumo);
+        leitor.close();
+
+    }   
+    static void calculoPlanos(double plan1, double plan2, double plan3,int minConsumo)
+    {
+        plan1 = minConsumo * 1.32 + 30;
+        plan2 = minConsumo * 1.12 + 40;
+        plan3 = minConsumo * 1.42 + 10;
+        
+        if(plan1 < plan2 && plan1 < plan2)
+        {
+            System.out.println("O primeiro plano é o mais barato! " + plan1);
+        }else if(plan2 < plan1 && plan2 < plan3)
+        {
+            System.out.println("O segundo plano é o mais barato! " + plan2);
+        }else if(plan3 < plan1 && plan3 < plan2)
+        {
+            System.out.println("O terceiro plano é o mais barato! " + plan3);
+        }else
+        {
+            System.out.println("Erro");
+        }     
+    } 
+}
