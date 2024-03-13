79dc5a652802065165ad5068354faa3efa38c335
Igor Daniel <igordaniel1903@gmail.com>
2024-03-13 18:19:12 -0300

    Estacionamento adicionado e alterado, TODO: adicionar uma funcao para o switch e melhorar os SOUT

--- Estacionamento/Main.java
+++ Estacionamento/Main.java
@@ -1,12 +1,78 @@
 package Estacionamento;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Carro c1 = new Carro("Compacto", "Fiat", "01239-123123", 2000, "azul");
-        Carro c2 = new Carro("Compacto", "Ford", "13231-323123", 2000, "azul");
+        String repetir;
+        Scanner input = new Scanner(System.in);  
+        String modelo;
+        String marca;
+        String placa;
+        int anoLanca;
+        String cor;
+        Carro c1 = null;
         CadastroCarro cc1 = new CadastroCarro();
-        cc1.inserir(c1);
-        cc1.inserir(c2);
-        
-        cc1.consultarCarro("01239-123123");
+
+        do{
+            menu();
+            int escolha = input.nextInt();
+            input.nextLine();
+            
+            switch (escolha) {
+                case 1:
+                    System.out.print("Modelo: ");
+                    modelo = input.nextLine();
+                    System.out.print("Marca: ");
+                    marca = input.nextLine();
+                    System.out.print("Placa: ");
+                    placa = input.nextLine();
+                    System.out.print("Ano de lançamento: ");
+                    anoLanca = input.nextInt();
+                    System.out.print("Cor: ");
+                    cor = input.nextLine();
+                    input.nextLine();
+
+                    c1 = new Carro(modelo, marca, placa, anoLanca, cor);
+                    break;
+            
+                case 2:
+                    String adicionaUltimo;
+                    System.out.print("Você deseja inserir o ultimo carro adicionado? (sim/nao)");
+                    adicionaUltimo = input.nextLine();
+                    if (adicionaUltimo.equals("sim")) {
+                        cc1.inserir(c1);
+                    }
+                    else{
+                        System.out.println("Carro não adicionado!");
+                    }
+                    break;
+                case 3:
+                    cc1.listarCarros();
+                    break;
+
+                case 4:
+                    System.out.println("Digite a placa do carro que você deseja remover: ");
+                    placa = input.nextLine();
+                    cc1.removerCarros(placa);
+                    break;
+                case 5:
+                    System.out.println("Digite a placa do carro que você deseja consultar: ");
+                    placa = input.nextLine();
+                    cc1.consultarCarro(placa);
+                    break;
+            }
+            System.out.println("Você deseja realizar outra operação? (sim/nao) ");
+            repetir = input.nextLine();
+        } while (repetir.equals("sim"));
+    }
+
+    public static void menu() {
+        System.out.print("\nOpções de sistema: "
+        + "\n 1- Criar carro" 
+        + "\n 2- Cadastrar carro" 
+        + "\n 3- Listar carros cadastrados" 
+        + "\n 4- Remover carro cadastrado "
+        + "\n 5- Consultar carro cadastrado"
+        + "\n\n Escolha: ");
     }
 }
